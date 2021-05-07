49c5c833734cb646220da2e96765597277f28ec2
selincandemir <candemir.selin@hotmail.com>
2021-05-07 23:10:08 +0300

    cleanup

--- xml_parse_methods.py
+++ xml_parse_methods.py
@@ -17,34 +17,9 @@ TODO:
 
 '''
 
-#import os
 import sqlite3
 con = sqlite3.connect('Enzymes.db')
 cur = con.cursor()
-
-# file_name = 'uniprot-ec__+AND+reviewed_yes.xml'
-# full_file = os.path.abspath(os.path.join(file_name))
-
-
-# def get_string_or_none(element_list, delimiter):
-#     list_text = ''
-#     try:
-#         for element in element_list:
-#             list_text += delimiter + element.text
-#         list_text = list_text.replace(delimiter, '', 1)
-#         if delimiter in list_text:
-#             list_text = '"' + list_text + '"'
-#     except AttributeError:
-#         print('attrib err')
-#     return list_text
-#
-#
-# def list_to_str(string_list, delimiter):
-#     joined_string = delimiter.join(string_list)
-#     if delimiter in joined_string:
-#         joined_string = '"' + joined_string + '"'
-#     return joined_string
-
 
 def process_entry(entry, ns):
     try:
