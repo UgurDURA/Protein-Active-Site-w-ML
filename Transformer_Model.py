926a98c770ea9f8a32ccf25d6f05ead4cac4cf71
selincandemir <candemir.selin@hotmail.com>
2021-05-20 21:56:51 +0300

    wip - embedding

--- Transformer_Model.py
+++ Transformer_Model.py
@@ -4,6 +4,7 @@ TODO: Transformer model tasks
 >[check] positional encoding
 >[] token embedding
 >[] combine embeddings
+> test detokenization?
 
 > encoding and decoding blocks
 
@@ -18,35 +19,39 @@ tok = tf.keras.preprocessing.text.Tokenizer(num_words=0, filters=None, lower=Fal
 
 # feed list of integers into fit_on_texts()
 # s: example sequence
-s = 'ANDJIDUVHEJELFJSUDGVKRLRRGMJIFUDN'
+s = 'MLPPWTLGLLLLATVRGKEVCYGQLGCFSDEKPWAGTLQRPVKLLPWSPEDIDTRFLLYTNENPNNFQLITGTEPDTIEASNFQLDRKTRFIIHGFLDKAEDSWPSDMCKKMFEVEKVNCICVDWRHGSRAMYTQAVQNIRVVGAETAFLIQALSTQLGYSLEDVHVIGHSLGAHTAAEAGRRLGGRVGRITGLDPAGPCFQDEPEEVRLDPSDAVFVDVIHTDSSPIVPSLGFGMSQKVGHLDFFPNGGKEMPGCKKNVLSTITDIDGIWEGIGGFVSCNHLRSFEYYSSSVLNPDGFLGYPCASYDEFQESKCFPCPAEGCPKMGHYADQFKGKTSAVEQTFFLNTGESGNFTSWRYKISVTLSGKEKVNGYIRIALYGSNENSKQYEIFKGSLKPDASHTCAIDVDFNVGKIQKVKFLWNKRGINLSEPKLGASQITVQSGEDGTEYNFCSSDTVEENVLQSLYPC '
+
 tok.fit_on_texts(s)
-print(tok.word_index)
-vocab_size = len(tok.word_index)
-
-# for every entry
-s = tok.texts_to_sequences(s)  # taking the string and turning it into a sequence of integers
 print(s)
-# print( tok.detokenize?(s) )
+s = tok.texts_to_sequences(s)
+print(s)
+vocab = len(tok.word_index)
+print(vocab)
 
-embed_dim = 10  # Embedding size for each token
+
+embed_dims = 10  # Embedding size for each token
 num_heads = 4  # Number of attention heads
 ff_dim = 10  # Hidden layer size in feed forward network inside transformer
 
 
-class embeddings(layers.Layer):
+class embeddings_layer(layers.Layer):
     """
-    Positional and token embedding in one layer
+    Positional encoding and token embedding in one layer
     """
 
-    def __init__(self, vocab_size, embed_dim):
-        # token embedding here?
-        # tok = tf.keras.preprocessing.text.Tokenizer(num_words=0, filters=None, lower=False, char_level=True)
+    def __init__(self, vocab_size, embed_dim):  # initialize with layer's attributes
+        super(embeddings_layer, self).__init__()    # ??
+        self.token_emb = layers.Embedding(input_dim=vocab_size, output_dim=embed_dim)
+        self.embed_dim = embed_dim
         print("init")
-        # self.token_enc = tf.keras.preprocessing.text.Tokenizer(num_words=0, filters=None, lower=False, char_level=True)
-        # self.pos_emb = layers.Embedding( )
 
-    def call(self, x):
+    def call(self, x):  # call with the input
+
+        pos = self.positional_encoding(len(x), self.embed_dim)
+        x = self.token_emb(x)  #fix input typing here!
         print("call")
+
+        return x + pos
 
     def get_angles(self, pos, i, d_model):
         angle_rates = 1 / np.power(10000, (2 * (i // 2)) / np.float32(d_model))
@@ -65,12 +70,11 @@ class embeddings(layers.Layer):
 
         pos_encoding = angle_rads[np.newaxis, ...]
 
-        return tf.cast(pos_encoding, dtype=tf.float32)      #output is in tensor form.
-
-    def token_embedding(self, ):
-        print("token embeddings")
-        # fill this up
+        return tf.cast(pos_encoding, dtype=tf.float32)  # output is in tensor form.
 
 
-imtesting = embeddings(tok.num_words, embed_dim)
-print(imtesting.positional_encoding(10, 5))
+imtesting = embeddings_layer(vocab, embed_dims)
+
+print(imtesting(s))
+# print(imtesting.positional_encoding(len(s), embed_dims))
+
