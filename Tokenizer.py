0522d58fa4946d148e0bb502b4b85000cd019b83
selincandemir <candemir.selin@hotmail.com>
2021-06-22 14:44:27 +0300

    tokenizer adjustments

--- Tokenizer.py
+++ Tokenizer.py
@@ -3,33 +3,46 @@ Tokenizers for input and output. Save the configurations in input_tok_config.txt
 for the purpose of using them to detokenize later.
 
 TODO:
-> read from database to train tokenizers
+>[check] read from database to train tokenizers
+    tokenizers: trained
 > save tokenized data to new table in database?
 
 '''
+
 import tensorflow as tf
 import json
+import sqlite3
 
+con = sqlite3.connect('[DATA]/db/Enzymes.db')
+cur = con.cursor()
+# connect to database, read 10 entries at a time and process them.
 
 # tokenizers
 input_tok = tf.keras.preprocessing.text.Tokenizer(num_words=0, filters=None, lower=False, char_level=True)
 output_tok = tf.keras.preprocessing.text.Tokenizer(num_words=0, filters='#$%&()+/<=>?@\\^{|}~', lower=False, split='.', char_level=False)
 
-# feed list of strings into fit_on_texts()
-# s: example sequence
-s = ['MLPPWTLGLLLLATVRGKEVCYGQLGCFSDEKPWAGTLQRPVKLLPWSPEDIDTRFLLYTNENPNNFQLITGTEPDTIEASNFQLDRKTRFIIHGFLDKAEDSWPSDMCKKMFEVEKVNCICVDWRHGSRAMYTQAVQNIRVVGAETAFLIQALSTQLGYSLEDVHVIGHSLGAHTAAEAGRRLGGRVGRITGLDPAGPCFQDEPEEVRLDPSDAVFVDVIHTDSSPIVPSLGFGMSQKVGHLDFFPNGGKEMPGCKKNVLSTITDIDGIWEGIGGFVSCNHLRSFEYYSSSVLNPDGFLGYPCASYDEFQESKCFPCPAEGCPKMGHYADQFKGKTSAVEQTFFLNTGESGNFTSWRYKISVTLSGKEKVNGYIRIALYGSNENSKQYEIFKGSLKPDASHTCAIDVDFNVGKIQKVKFLWNKRGINLSEPKLGASQITVQSGEDGTEYNFCSSDTVEENVLQSLYPC ',
-'MIGRLNHVAIAVPDLEAAAAQYRNTLGAEVGAPQDEPDHGVTVIFITLPNTKIELLHPLGEGSPIAGFLEKNPAGGIHHICYEVEDILAARDRLKEAGARVLGSGEPKIGAHGKPVLFLHPKDFNGCLVELEQV']
-ec = ['4.1.1.15', '5.1.99.-']
+cur.execute('SELECT sequence_string, ec_number_string FROM Entries')
+while True:
+    batch = cur.fetchmany(10) # batch is list of tuples
 
-input_tok.fit_on_texts(s)
-output_tok.fit_on_texts(ec)
+    s = [i[0] for i in batch]
+    ec = [n[1] for n in batch]
+
+    input_tok.fit_on_texts(s)
+    output_tok.fit_on_texts(ec)
+
+    if not batch:
+        break
+    # each batch contains up to 10 rows
+
 
 input_config = input_tok.to_json()
 output_config = output_tok.to_json()
 
-with open('input_tok_config.txt', 'w') as outfile:
+with open('saves\input_tok_config.txt', 'w') as outfile:
     json.dump(input_config, outfile)
 
-with open('output_tok_config.txt', 'w') as outfile:
+with open('saves\output_tok_config.txt', 'w') as outfile:
     json.dump(output_config, outfile)
 
+con.close()
