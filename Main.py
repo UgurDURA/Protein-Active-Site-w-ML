c4777dcab840f68e8cb8ab7ab127f6b219d6c8b4
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2021-06-29 00:07:21 +0300

    Data Analysis Implemented

--- Main.py
+++ Main.py
@@ -38,23 +38,56 @@ dataset= pd.read_csv('[DATA]/DB/Entries.csv')  #taking data from csv file, you c
 Ec_Number = dataset.iloc[:,2].values #features
 Sequence = dataset.iloc[:,-1].values  #Dependent values      #a better name could be suggested
 
+
 print(Ec_Number)
 print(Sequence)
+
+#Data Analysis
+ 
+count_aminos={}
+length_seqs=[]
+for i, seq in enumerate(Sequence):
+    length_seqs.append(len(seq))
+    for a in seq:
+        if a in count_aminos:
+            count_aminos[a] += 1
+        else:
+            count_aminos[a] = 0
+
+unique_aminos=list(count_aminos.keys())
+
+print('Unique aminos ({}):\n{}'.format(len(unique_aminos), unique_aminos))
+x=[i for i in range(len(unique_aminos))]
+plt.bar(x, count_aminos.values())
+plt.xticks(x, unique_aminos)
+print(list(count_aminos.values())[-5:])
+plt.show()
+
+
+print('Average length:', np.mean(length_seqs))
+print('Deviation:', np.std(length_seqs))
+print('Min length:', np.min(length_seqs))
+print('Max length:', np.max(length_seqs))
+
+print('Average length:', np.mean(length_seqs))
+print('Deviation:', np.std(length_seqs))
+print('Min length:', np.min(length_seqs))
+print('Max length:', np.max(length_seqs))
 
 #Split dataset into test and validation
 
 #Important note for here, the split need to be performed after tokenization and embedding
 #I just added the code here 
 
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 
-Ec_Number_train,Ec_Number_test,Sequence_train,Sequence_test =train_test_split(Ec_Number,Sequence,test_size=0.2, random_state=1)
+# Ec_Number_train,Ec_Number_test,Sequence_train,Sequence_test =train_test_split(Ec_Number,Sequence,test_size=0.2, random_state=1)
 
 
-print(Ec_Number_train)
-print(Ec_Number_test)
-print(Sequence_train)
-print(Sequence_test)
+# print(Ec_Number_train)
+# print(Ec_Number_test)
+# print(Sequence_train)
+# print(Sequence_test)
 
 # def applyMask(dirtyData, dirty_idxs):
 #     if (type(dirtyData)!=type(np.asarray([]))):
