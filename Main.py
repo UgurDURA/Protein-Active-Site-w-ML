7cb5b1ccff35f336314758d443a2e6685e128e5e
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2021-06-30 19:46:11 +0300

    reverted

---
+++ Main.py
@@ -0,0 +1,152 @@
+
+#This file created for trial
+
+
+# To do list
+
+'1- DATA PREPROCESSING '
+' 1.1 Access the Data'
+' 1.2 Spit the data into features and depandents'
+' 1.3 At the end of the preprocessing the data split it into Test and Validation'
+'2- TOKENIZATION'
+' 2.1 BPE Algorithym'
+' 2.2 '
+
+
+import collections
+import logging
+import os
+import pathlib
+import re
+import string
+import sys
+import time
+
+import numpy as np
+import pandas as pd
+import scipy as sp
+# import sentencepiece as spm
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import json
+from tensorflow.keras import layers
+
+logging.getLogger('tensorflow').setLevel(logging.ERROR)  # suppress warnings
+
+
+dataset= pd.read_csv('[DATA]/DB/Entries.csv')  #taking data from csv file, you can easily export the data from SQL file to csv
+Ec_Number = dataset.iloc[:,2].values #features
+Sequence = dataset.iloc[:,-1].values  #Dependent values      #a better name could be suggested
+
+
+print(Ec_Number)
+print(Sequence)
+
+
+'Data Analysis'
+ 
+count_aminos={}
+length_seqs=[]
+for i, seq in enumerate(Sequence):
+    length_seqs.append(len(seq))
+    for a in seq:
+        if a in count_aminos:
+            count_aminos[a] += 1
+        else:
+            count_aminos[a] = 0
+
+unique_aminos=list(count_aminos.keys())
+
+print('Unique aminos ({}):\n{}'.format(len(unique_aminos), unique_aminos))
+x=[i for i in range(len(unique_aminos))]
+plt.bar(x, count_aminos.values())
+plt.xticks(x, unique_aminos)
+print(list(count_aminos.values())[-5:])
+plt.show()
+
+
+print('Average length:', np.mean(length_seqs))
+print('Deviation:', np.std(length_seqs))
+print('Min length:', np.min(length_seqs))
+print('Max length:', np.max(length_seqs))
+
+print('Average length:', np.mean(length_seqs))
+print('Deviation:', np.std(length_seqs))
+print('Min length:', np.min(length_seqs))
+print('Max length:', np.max(length_seqs))
+
+'Split dataset into test and validation'
+
+#Important note for here, the split need to be performed after tokenization and embedding
+#I just added the code here 
+
+from sklearn.model_selection import train_test_split
+
+Ec_Number_train,Ec_Number_test,Sequence_train,Sequence_test =train_test_split(Ec_Number,Sequence,test_size=0.2, random_state=1)
+
+
+# print(Ec_Number_train)
+# print(Ec_Number_test)
+# print(Sequence_train)
+# print(Sequence_test)
+
+SequenceTestFile = open("SequenceTest.txt", "w")
+for element in Sequence_test:
+    SequenceTestFile.write(element + "\n")
+SequenceTestFile.close()
+
+print('Sequence Test Data Created Succesfully....')
+
+EcNumberTestFile = open("Ec_NumbersTest.txt", "w")
+for element in Ec_Number_test:
+    EcNumberTestFile.write(element + "\n")
+EcNumberTestFile.close()
+
+print('Ec Number Test Data Created Succesfully....')
+
+SequenceTrainingFile = open("SequencesTraining.txt", "w")
+for element in Sequence_train:
+    SequenceTrainingFile.write(element + "\n")
+SequenceTrainingFile.close()
+
+print('Sequence Training Data Created Succesfully....')
+
+EcNumberTrainingFile = open("Ec_NumbersTraining.txt", "w")
+for element in Ec_Number_train:
+    EcNumberTrainingFile.write(element + "\n")
+EcNumberTrainingFile.close()
+
+print('Ec Number Training Data Created Succesfully....')
+# def applyMask(dirtyData, dirty_idxs):
+#     if (type(dirtyData)!=type(np.asarray([]))):
+#         dirtyData=np.asarray(dirtyData)
+
+#     returnData= dirtyData[np.logical_not(dirty_idxs)]
+
+#     return returnData
+
+# #Tokenization 
+
+# path  = 'all.tab'
+# int_path = 'interact.json'
+# seq_path = 'pretraining_data.txt'
+# model_path = 'm_reviewed.model'
+
+# def filter_seqs():
+# 	"""
+# 	Filter sequences by length
+# 	"""
+	
+# 	seq_ls = [seq for seq in dataset['sequence_string'] if len(seq)<1024]
+# 	with open(seq_path, 'w') as filehandle:
+# 		for listitem in seq_ls:
+# 			filehandle.write('%s\n' % listitem)
+
+
+ 
+# from transformer import BertForMaskedLM, BertTokenizer, pipeline
+# tokenizer = BertTokenizer.from_pretrained("Rostlab/prot_bert", do_lower_case=False )
+# model = BertForMaskedLM.from_pretrained("Rostlab/prot_bert")
+# unmasker = pipeline('fill-mask', model=model, tokenizer=tokenizer)
+# unmasker('D L I P T S S K L V V [MASK] D T S L Q V K K A F F A L V T')
+ 
--- Trial/TrainTokenizer.py
+++ Trial/TrainTokenizer.py
@@ -1,19 +1,18 @@
 #Install r-tokenizers and r-tokenizers.bpe on conda environment
-from torch.utils.data import Dataset
+
 from pathlib import Path
 import sqlite3
 from tokenizers import ByteLevelBPETokenizer
 from tokenizers.processors import BertProcessing
 
-con = sqlite3.connect('[DATA]/db/Enzymes.db')
-cur = con.cursor()
+# con = sqlite3.connect('[DATA]/db/Enzymes.db')
+# cur = con.cursor()
 
 
-cur.execute('SELECT sequence_string, ec_number_string FROM Entries')
+# cur.execute('SELECT sequence_string, ec_number_string FROM Entries')
 
 # f = open("Sequences.txt", "w")
 # f = open("Ec_Numbers.txt", "w")
-
 
 # while True:
 
@@ -37,8 +36,7 @@ cur.execute('SELECT sequence_string, ec_number_string FROM Entries')
 
 #     if not batch:
 #         break
-  
-
+    
     
 # paths = [str(x) for x in Path("[DATA]\DB\Sequences.txt").glob("**/*.txt")] 
 
@@ -58,56 +56,16 @@ cur.execute('SELECT sequence_string, ec_number_string FROM Entries')
 # con.close()
 
 
-# tokenizer = ByteLevelBPETokenizer(
-#     "Models\SequenceTokens-vocab.json",
-#     "Models\SequenceTokens-merges.txt",
-# )
-# tokenizer._tokenizer.post_processor = BertProcessing(
-#     ("</s>", tokenizer.token_to_id("</s>")),
-#     ("<s>", tokenizer.token_to_id("<s>")),
-# )
-
-# print(tokenizer.encode("LLQPLVFSSVLSWIPQTGVIFINLVVCWSYYAYVVELCI")
-
-# )
-
-class EsperantoDataset(Dataset):
-    def __init__(self, evaluate: bool = False):
-        tokenizer = ByteLevelBPETokenizer(
-            "Models\SequenceTokens-vocab.json",
-            "Models\SequenceTokens-merges.txt",
-        )
-        tokenizer._tokenizer.post_processor = BertProcessing(
-            ("</s>", tokenizer.token_to_id("</s>")),
-            ("<s>", tokenizer.token_to_id("<s>")),
-        )
-        tokenizer.enable_truncation(max_length=35000)
-        # or use the RobertaTokenizer from `transformers` directly.
-
-        self.examples = []
-
-        src_files = Path("[DATA]\TestData\SequenceTest.txt").glob("*-eval.txt") if evaluate else Path("[DATA]\TrainingData\SequencesTraining.txt").glob("*-train.txt")
-        for src_file in src_files:
-            print("🔥", src_file)
-            lines = src_file.read_text(encoding="utf-8").splitlines()
-            self.examples += [x.ids for x in tokenizer.encode_batch(lines)]
-
-    def __len__(self):
-        return len(self.examples)
-
-    def __getitem__(self, i):
-        # We’ll pad at the batch level.
-        return torch.tensor(self.examples[i])
-
-from transformers import pipeline
-
-fill_mask = pipeline(
-    "fill-mask",
-    model="./models/EsperBERTo-small",
-    tokenizer="./models/EsperBERTo-small"
+tokenizer = ByteLevelBPETokenizer(
+    "Models\SequenceTokens-vocab.json",
+    "Models\SequenceTokens-merges.txt",
+)
+tokenizer._tokenizer.post_processor = BertProcessing(
+    ("</s>", tokenizer.token_to_id("</s>")),
+    ("<s>", tokenizer.token_to_id("<s>")),
 )
 
-result = fill_mask("La suno <mask>.")
+print(tokenizer.encode("LLQPLVFSSVLSWIPQTGVIFINLVVCWSYYAYVVELCI")
 
+)
 
-print('Hello world') 
