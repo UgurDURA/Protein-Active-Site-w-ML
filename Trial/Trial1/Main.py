2a4fab64f9c7c047d47bc06f7fdd2803257eaaf4
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2021-07-03 01:59:25 +0300

    Xids, Xmasks and labels added

--- Trial/Trial1/Main.py
+++ Trial/Trial1/Main.py
@@ -41,49 +41,49 @@ dataset= pd.read_csv('[DATA]\DummyData\TestData.csv')  #taking data from csv fil
 
 TraininDataset=pd.read_csv('[DATA]\TrainingData\TrainDataset.csv') 
 
-EcNumberDataset =list(TraininDataset.iloc[:,1])#features
-SequenceDataset =list(TraininDataset.iloc[:,2])   #Dependent values  
+EcNumberDataset =list(TraininDataset.iloc[:,2])#features
+SequenceDataset =list(TraininDataset.iloc[:,1])   #Dependent values  
 
-print(EcNumberDataset)
-print(SequenceDataset)
+# print(EcNumberDataset)
+# print(SequenceDataset)
 
 'Data Analysis'
 
-count_aminos={}
-SequenceSize=len(dataset.iloc[:,-1])
+# count_aminos={}
+# SequenceSize=len(dataset.iloc[:,-1])
 
-length_seqs=[]
-for i, seq in enumerate(dataset.iloc[:,-1]):
-    length_seqs.append(len(seq))
-    for a in seq:
-        if a in count_aminos:
-            count_aminos[a] += 1
-        else:
-            count_aminos[a] = 0
+# length_seqs=[]
+# for i, seq in enumerate(dataset.iloc[:,-1]):
+#     length_seqs.append(len(seq))
+#     for a in seq:
+#         if a in count_aminos:
+#             count_aminos[a] += 1
+#         else:
+#             count_aminos[a] = 0
 
-unique_aminos=list(count_aminos.keys())
+# unique_aminos=list(count_aminos.keys())
 
-print('Unique aminos ({}):\n{}'.format(len(unique_aminos), unique_aminos))
-x=[i for i in range(len(unique_aminos))]
-plt.bar(x, count_aminos.values())
-plt.xticks(x, unique_aminos)
-print(list(count_aminos.values())[-5:])
-plt.show()
+# print('Unique aminos ({}):\n{}'.format(len(unique_aminos), unique_aminos))
+# x=[i for i in range(len(unique_aminos))]
+# plt.bar(x, count_aminos.values())
+# plt.xticks(x, unique_aminos)
+# print(list(count_aminos.values())[-5:])
+# plt.show()
 
 
-print('Average length:', np.mean(length_seqs))
-print('Deviation:', np.std(length_seqs))
-print('Min length:', np.min(length_seqs))
-print('Max length:', np.max(length_seqs))
+# print('Average length:', np.mean(length_seqs))
+# print('Deviation:', np.std(length_seqs))
+# print('Min length:', np.min(length_seqs))
+# print('Max length:', np.max(length_seqs))
 
-sorted_seqs=np.array(length_seqs)
-sorted_seqs.sort()
-print('10 shortest:\n{}\n10 longest:\n{}'.format(sorted_seqs[:10], sorted_seqs[-10:]))
+# sorted_seqs=np.array(length_seqs)
+# sorted_seqs.sort()
+# print('10 shortest:\n{}\n10 longest:\n{}'.format(sorted_seqs[:10], sorted_seqs[-10:]))
 
-print("Number of Sequences: ", SequenceSize)
-print('Number sequences less than 30 AA:', len(sorted_seqs[sorted_seqs<30]))
-print('Number sequences more than 500 AA:', len(sorted_seqs[sorted_seqs>500]))
-print('Number sequences more than 1000 AA:', len(sorted_seqs[sorted_seqs>1000]))
+# print("Number of Sequences: ", SequenceSize)
+# print('Number sequences less than 30 AA:', len(sorted_seqs[sorted_seqs<30]))
+# print('Number sequences more than 500 AA:', len(sorted_seqs[sorted_seqs>500]))
+# print('Number sequences more than 1000 AA:', len(sorted_seqs[sorted_seqs>1000]))
 
 # density={}
 
@@ -111,66 +111,66 @@ print('Number sequences more than 1000 AA:', len(sorted_seqs[sorted_seqs>1000]))
 # plt.title('Sequence Size Distribution')
 
 # plt.show()
-Optimized_length_seq=[]
+# Optimized_length_seq=[]
 
-for item in length_seqs:
-    if(item<1000):
-        Optimized_length_seq.append(item)
+# for item in length_seqs:
+#     if(item<1000):
+#         Optimized_length_seq.append(item)
     
 
-N_points = 10000
-n_bins = 200
-legend = ['distribution']
+# N_points = 10000
+# n_bins = 200
+# legend = ['distribution']
 
-fig, axs = plt.subplots(1, 1,
-    figsize =(10, 7), 
-    tight_layout = True)
+# fig, axs = plt.subplots(1, 1,
+#     figsize =(10, 7), 
+#     tight_layout = True)
   
   
-# Remove axes splines 
-for s in ['top', 'bottom', 'left', 'right']: 
-    axs.spines[s].set_visible(False) 
+# # Remove axes splines 
+# for s in ['top', 'bottom', 'left', 'right']: 
+#     axs.spines[s].set_visible(False) 
   
-# Remove x, y ticks
-axs.xaxis.set_ticks_position('none') 
-axs.yaxis.set_ticks_position('none') 
+# # Remove x, y ticks
+# axs.xaxis.set_ticks_position('none') 
+# axs.yaxis.set_ticks_position('none') 
     
-# Add padding between axes and labels 
-axs.xaxis.set_tick_params(pad = 5) 
-axs.yaxis.set_tick_params(pad = 10) 
+# # Add padding between axes and labels 
+# axs.xaxis.set_tick_params(pad = 5) 
+# axs.yaxis.set_tick_params(pad = 10) 
   
-# Add x, y gridlines 
-axs.grid(b = True, color ='grey', 
-        linestyle ='-.', linewidth = 0.5, 
-        alpha = 0.6) 
+# # Add x, y gridlines 
+# axs.grid(b = True, color ='grey', 
+#         linestyle ='-.', linewidth = 0.5, 
+#         alpha = 0.6) 
   
-# Add Text watermark 
-fig.text(0.9, 0.15, 'Proten Function Prediction', 
-         fontsize = 12, 
-         color ='red',
-         ha ='right',
-         va ='bottom', 
-         alpha = 0.7) 
+# # Add Text watermark 
+# fig.text(0.9, 0.15, 'Proten Function Prediction', 
+#          fontsize = 12, 
+#          color ='red',
+#          ha ='right',
+#          va ='bottom', 
+#          alpha = 0.7) 
   
-# Creating histogram
-N, bins, patches = axs.hist(Optimized_length_seq, bins = n_bins)
+# # Creating histogram
+# N, bins, patches = axs.hist(Optimized_length_seq, bins = n_bins)
   
-# Setting color
-fracs = ((N**(1 / 5)) / N.max())
-norm = colors.Normalize(fracs.min(), fracs.max())
+# # Setting color
+# fracs = ((N**(1 / 5)) / N.max())
+# norm = colors.Normalize(fracs.min(), fracs.max())
   
-for thisfrac, thispatch in zip(fracs, patches):
-    color = plt.cm.viridis(norm(thisfrac))
-    thispatch.set_facecolor(color)
+# for thisfrac, thispatch in zip(fracs, patches):
+#     color = plt.cm.viridis(norm(thisfrac))
+#     thispatch.set_facecolor(color)
   
-# Adding extra features    
-plt.xlabel("Sequence Length")
-plt.ylabel("Number of Sequences")
-plt.legend(legend)
-plt.title('Sequence Length Distribution')
+# # Adding extra features    
+# plt.xlabel("Sequence Length")
+# plt.ylabel("Number of Sequences")
+# plt.legend(legend)
+# plt.title('Sequence Length Distribution')
   
-# Show plot
-plt.show()
+# # Show plot
+# plt.show()
 
 
 
@@ -216,15 +216,18 @@ print(Xids)
 print(Xmask)
 
 
- 
+print(TraininDataset.iloc[:,2].unique)
 
-arr=EcNumberDataset
+arr=TraininDataset.iloc[:,2].values
+
+print(arr.size)
 
 labels=np.zeros((arr.size,arr.max()+1))
 
 print(labels.shape)
 
-
+labels[np.arange(arr.size),arr]=1
+print(labels)
 
 
 
