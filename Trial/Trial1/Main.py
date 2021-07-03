0b227427e0a56283813b2ca3a617f68a631e475e
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2021-07-04 02:27:02 +0300

    Update Main.py

--- Trial/Trial1/Main.py
+++ Trial/Trial1/Main.py
@@ -1,5 +1,4 @@
-
-#This file is created for experimentation
+# This file is created for experimentation
 
 # To do list
 '1- DATA PREPROCESSING '
@@ -10,46 +9,25 @@
 ' 2.1 BPE Algorithym'
 ' 2.2 '
 
-from ast import increment_lineno
-import collections
-from imp import SEARCH_ERROR
 import logging
-import os
-import pathlib
-import re
-import string
-import sys
-import time
-from matplotlib import colors
-
 import numpy as np
-from numpy.core.fromnumeric import shape
-from numpy.lib.histograms import histogram
 import pandas as pd
-import scipy as sp
-# import sentencepiece as spm
-import matplotlib.pyplot as plt
-import seaborn as sns
 import tensorflow as tf
-import json
-import sqlite3
-from tensorflow.keras import layers
-from tensorflow.python.keras.backend import dtype
-from tensorflow.python.keras.callbacks import History
-from tensorflow.python.ops.gen_logging_ops import Print
+from transformers import BertTokenizer
 
- 
- 
 logging.getLogger('tensorflow').setLevel(logging.ERROR)  # suppress warnings
 
+dataset = pd.read_csv('[DATA]\DB\MainDataset\MainDataset.csv')  # taking data from csv file, you can easily export the data from SQL file to csv
 
-dataset= pd.read_csv('[DATA]\DB\MainDataset\MainDataset.csv')  #taking data from csv file, you can easily export the data from SQL file to csv
-
-TraininDataset=pd.read_csv('[DATA]\TrainingData\TrainDataset.csv') 
-
+<<<<<<< HEAD
 EcNumberDataset =list(dataset.iloc[:,4])   #features
 SequenceDataset =list(dataset.iloc[:,5])   #Dependent values  
+=======
+TrainingDataset = pd.read_csv('[DATA]\TrainingData\TrainDataset.csv')
+>>>>>>> 74cb52a06485cf7b047cd604ff2dc9f5d51a7da8
 
+EcNumberDataset = list(dataset.iloc[:, 4])  # features
+SequenceDataset = list(dataset.iloc[:, 5])  # Dependent values
 
 # print(EcNumberDataset)
 # print(SequenceDataset)
@@ -99,17 +77,16 @@ SequenceDataset =list(dataset.iloc[:,5])   #Dependent values
 #     upper=len(sorted_seqs[sorted_seqs>i+2])
 
 #     print ("Lower  ",lower,"    ","Upper   ",upper,"       ","Sequence Length",np.max(length_seqs))
-    
+
 #     calc=(SequenceSize)-abs(lower)-abs(upper)
 #     calc=abs(calc)
-    
+
 #     density[i]=calc
 #     # print(i, " / ", np.max(length_seqs))
 
 # lists = sorted(density.items()) # sorted by key, return a list of tuples
 # print(density)
 # x, y = zip(*lists) # unpack a list of pairs into two tuples
-
 
 
 # plt.plot(x, y)
@@ -123,7 +100,7 @@ SequenceDataset =list(dataset.iloc[:,5])   #Dependent values
 # for item in length_seqs:
 #     if(item<1000):
 #         Optimized_length_seq.append(item)
-    
+
 
 # N_points = 10000
 # n_bins = 200
@@ -132,25 +109,25 @@ SequenceDataset =list(dataset.iloc[:,5])   #Dependent values
 # fig, axs = plt.subplots(1, 1,
 #     figsize =(10, 7), 
 #     tight_layout = True)
-  
-  
+
+
 # # Remove axes splines 
 # for s in ['top', 'bottom', 'left', 'right']: 
 #     axs.spines[s].set_visible(False) 
-  
+
 # # Remove x, y ticks
 # axs.xaxis.set_ticks_position('none') 
 # axs.yaxis.set_ticks_position('none') 
-    
+
 # # Add padding between axes and labels 
 # axs.xaxis.set_tick_params(pad = 5) 
 # axs.yaxis.set_tick_params(pad = 10) 
-  
+
 # # Add x, y gridlines 
 # axs.grid(b = True, color ='grey', 
 #         linestyle ='-.', linewidth = 0.5, 
 #         alpha = 0.6) 
-  
+
 # # Add Text watermark 
 # fig.text(0.9, 0.15, 'Proten Function Prediction', 
 #          fontsize = 12, 
@@ -158,28 +135,29 @@ SequenceDataset =list(dataset.iloc[:,5])   #Dependent values
 #          ha ='right',
 #          va ='bottom', 
 #          alpha = 0.7) 
-  
+
 # # Creating histogram
 # N, bins, patches = axs.hist(Optimized_length_seq, bins = n_bins)
-  
+
 # # Setting color
 # fracs = ((N**(1 / 5)) / N.max())
 # norm = colors.Normalize(fracs.min(), fracs.max())
-  
+
 # for thisfrac, thispatch in zip(fracs, patches):
 #     color = plt.cm.viridis(norm(thisfrac))
 #     thispatch.set_facecolor(color)
-  
+
 # # Adding extra features    
 # plt.xlabel("Sequence Length")
 # plt.ylabel("Number of Sequences")
 # plt.legend(legend)
 # plt.title('Sequence Length Distribution')
-  
+
 # # Show plot
 # plt.show()
 
 
+<<<<<<< HEAD
 
 
 # "Tokenization "
@@ -193,25 +171,42 @@ print('Example Sequence:    ',Sequence_Example, " Example EC number:  ",Ec_Numbe
 from transformers import AutoTokenizer 
 
 MAX_LEN=512
+=======
+"Tokenization "
+>>>>>>> 74cb52a06485cf7b047cd604ff2dc9f5d51a7da8
 
+Sequence_Example = SequenceDataset[1]
+Ec_NumberExample = EcNumberDataset[1]
 
+print('Example Sequence:    ', Sequence_Example, " Example EC number:  ", Ec_NumberExample)
 
+<<<<<<< HEAD
 tokenizer = AutoTokenizer.from_pretrained('Rostlab/prot_bert_bfd', do_lower_case=False, )
 tokens=tokenizer.encode_plus(Sequence_Example, max_length=MAX_LEN,truncation=True,padding="max_length",
                                 add_special_tokens=True,return_token_type_ids=False,return_attention_mask=True, return_tensors='tf')
 
+=======
+MAX_LEN = 512
+
+tokenizer = BertTokenizer.from_pretrained('Rostlab/prot_bert_bfd_localization', do_lower_case=False, )
+tokens = tokenizer.encode_plus(Sequence_Example, max_length=MAX_LEN, truncation=True, padding="max_length",
+                               add_special_tokens=True, return_token_type_ids=False, return_attention_mask=True, return_tensors='tf')
+>>>>>>> 74cb52a06485cf7b047cd604ff2dc9f5d51a7da8
 
 print("TOKENS")
 print(tokens)
 
-
-Xids= np.zeros((len(dataset),MAX_LEN))
-Xmask= np.zeros((len(dataset),MAX_LEN))
+Xids = np.zeros((len(dataset), MAX_LEN))
+Xmask = np.zeros((len(dataset), MAX_LEN))
 
 print("XIDS SHAPE")
 print(Xids.shape)
 
+for i, sequence in enumerate(dataset.iloc[:, 5]):
+    tokens = tokenizer.encode_plus(sequence, max_length=MAX_LEN, truncation=True, padding="max_length",
+                                   add_special_tokens=True, return_token_type_ids=False, return_attention_mask=True, return_tensors='tf')
 
+<<<<<<< HEAD
 
 
 
@@ -223,29 +218,42 @@ for i, sequence in enumerate (dataset.iloc[:,5]):
     Xids[i,:], Xmask[i,:]= tokens['input_ids'], tokens['attention_mask']
 
 
+=======
+    Xids[i, :], Xmask[i, :] = tokens['input_ids'], tokens['attention_mask']
+>>>>>>> 74cb52a06485cf7b047cd604ff2dc9f5d51a7da8
 print("XIDS")
 print(type(Xids))
 print("XMASKS")
 print(Xmask)
 
+print(dataset.iloc[:, 4].unique)
 
+<<<<<<< HEAD
 print(dataset['ECNumber'].unique)
 
 arr=dataset['ECNumber'].values
+=======
+arr = dataset.iloc[:, 4].values
+>>>>>>> 74cb52a06485cf7b047cd604ff2dc9f5d51a7da8
 
 print("Array Size")
 print(arr.size)
 
+<<<<<<< HEAD
 labels=np.zeros((arr.size, arr.max()+1))
+=======
+labels = np.zeros((arr.size, arr.max() + 1))
+>>>>>>> 74cb52a06485cf7b047cd604ff2dc9f5d51a7da8
 
 print("Labels Shape")
 print(labels.shape)
 
-labels[np.arange(arr.size),arr]=1
+labels[np.arange(arr.size), arr] = 1
 
 print("LABELS")
 print(labels)
 
+<<<<<<< HEAD
 
 # Below code is for off loading the data
 
@@ -268,11 +276,14 @@ with open('labels.npy','wb') as f:
 
 # with open('labels.npy','rb') as fp:
 #     labels=np.load(fp)
+=======
+# Below code is for off loading the data
+>>>>>>> 74cb52a06485cf7b047cd604ff2dc9f5d51a7da8
 
 
 tf.config.experimental.list_physical_devices('GPU')
 
-tensorflow_dataset=tf.data.Dataset.from_tensor_slices((Xids,Xmask,labels))
+tensorflow_dataset = tf.data.Dataset.from_tensor_slices((Xids, Xmask, labels))
 
 print("DATASET ON TENSOR FLOW EXAMPLE")
 for i in tensorflow_dataset.take(1):
@@ -280,39 +291,60 @@ for i in tensorflow_dataset.take(1):
 
 
 def map_func(input_ids, masks, labels):
+<<<<<<< HEAD
     return{'input_ids': input_ids,'attention_mask': masks},labels
+=======
+    return {'input_ids': input_ids, 'attention mask': masks}, labels
+>>>>>>> 74cb52a06485cf7b047cd604ff2dc9f5d51a7da8
 
 
-tensorflow_dataset=tensorflow_dataset.map(map_func)
+tensorflow_dataset = tensorflow_dataset.map(map_func)
 
 for i in tensorflow_dataset.take(1):
     print(i)
 
+tensorflow_dataset = tensorflow_dataset.shuffle(1000000).batch(32)
 
+<<<<<<< HEAD
 tensorflow_dataset=tensorflow_dataset.shuffle(100000).batch(32)
 
 DS_LEN=len(list(tensorflow_dataset))
+=======
+DS_LEN = len(list(tensorflow_dataset))
+>>>>>>> 74cb52a06485cf7b047cd604ff2dc9f5d51a7da8
 
 print(DS_LEN)
 
-SPLIT=.9
+SPLIT = .9
 
+<<<<<<< HEAD
 train= tensorflow_dataset.take(round(DS_LEN*SPLIT))
 val=tensorflow_dataset.skip(round(DS_LEN*SPLIT))
+=======
+train = tensorflow_dataset.take(round(DS_LEN=SPLIT))
+val = tensorflow_dataset.skip(round(DS_LEN=SPLIT))
+>>>>>>> 74cb52a06485cf7b047cd604ff2dc9f5d51a7da8
 
 del tensorflow_dataset
 
 from transformers import TFAutoModel
 from tensorflow import keras
 
+<<<<<<< HEAD
 bert= TFAutoModel.from_pretrained('Rostlab/prot_bert_bfd')
 
 input_ids=tf.keras.layers.Input(shape=(MAX_LEN,),name='input_ids', dtype='int32')
 mask=tf.keras.layers.Input(shape=(MAX_LEN,),name='attention_mask', dtype='int32')
+=======
+bert = TFAutoModel.from_pretrained('Rostlab/prot_bert_bfd_localization')
+>>>>>>> 74cb52a06485cf7b047cd604ff2dc9f5d51a7da8
 
-embeddings=bert(input_ids, attention_mask=mask)[0]
+input_ids = tf.keras.layers.Input(shape=(MAX_LEN), name='input_ids', dtype='int32')
+mask = tf.keras.layers.Input(shape=(MAX_LEN), name='attention_mask', dtype='int32')
 
+embeddings = bert(input_ids, attention_mask=mask)[0]
 
+<<<<<<< HEAD
 X=tf.keras.layers.GlobalMaxPooling1D()(embeddings)
 X=tf.keras.layers.BatchNormalization()(X)
 X=tf.keras.layers.Dense(128,activation='relu')(X)
@@ -329,10 +361,26 @@ optimizer= tf.keras.optimizers.Adam(0.01)
 loss= tf.keras.losses.CategoricalCrossentropy()
 acc= tf.keras.metrics.CategoricalAccuracy('accuracy')
 
+=======
+X = tf.keras.layers.GlobalMaxPooling1D()(embeddings)
+X = tf.keras.layers.BatchNormalization()(X)
+X = tf.keras.layers.Dense(128, activation='relu')(X)
+X = tf.keras.layers.Dropout(0.1)(X)
+X = tf.keras.layers.Dense(32, activation='relu')(X)
+X = tf.keras.layers.Dense(7, activation='softmax', name='outputs')(X)
+
+model = tf.keras.Model(intputs=[input_ids, mask], outputs=y)
+
+model.summary()
+
+optimizer = tf.keras.optimizer.Adam(0.01)
+loss = tf.keras.losses.CategoricalCrossentropy()
+acc = tf.keras.metrics.CategoricalAccuracy('accuracy')
+>>>>>>> 74cb52a06485cf7b047cd604ff2dc9f5d51a7da8
 
 model.compile(optimizer=optimizer, loss=loss, metrics=[acc])
 
-history=model.fit(
+history = model.fit(
 
     train,
     validation_data=val,
