8c83ae343d791b0e779c337cb8be3d5378f8c00e
selincandemir <candemir.selin@hotmail.com>
2021-07-11 00:14:56 +0300

    cleaning - model_init.py added

---
+++ Modules/EC_Prediction_Modules/EC_First/model_init.py
@@ -0,0 +1,54 @@
+from transformers import AutoTokenizer, TFAutoModel, TFBertModel, XLNetTokenizer, TFXLNetForSequenceClassification
+import tensorflow as tf
+from .... import *
+
+def create_model(**kwargs):
+    embedding_base = kwargs.embedding_base      # specify ProtBERT_BFD or XLNET
+    categories = kwargs.categories              # number of labels
+
+    # acrobatics to avoid putting a model inside a model in keras which prevents saving the model
+    if embedding_base == "ProtBERT_BFD":
+        base = TFAutoModel.from_pretrained('Rostlab/prot_bert_bfd')
+        assert isinstance(base, TFBertModel)
+        main_layer = base.bert
+
+        input_ids = tf.keras.layers.Input(shape=(MAX_LEN,), name='input_ids', dtype='int32')
+        mask = tf.keras.layers.Input(shape=(MAX_LEN,), name='attention_mask', dtype='int32')
+
+        embeddings = main_layer(input_ids, attention_mask=mask)
+
+    elif embedding_base == "XLNET":
+        base = TFAutoModel.from_pretrained("Rostlab/prot_xlnet", from_pt=True)
+        assert isinstance(base, TFXLNetForSequenceClassification)
+        main_layer = base.xlnet
+
+        inputs = tf.keras.layers.Input(shape=None, name="input layer", ragged=True, type_spec=None)
+        embeddings = main_layer(inputs)[0]
+
+    else:
+        print("invalid arg")
+        # throw error
+    del base
+
+    X = tf.keras.layers.GlobalMaxPooling1D()(embeddings)
+    X = tf.keras.layers.BatchNormalization()(X)
+    X = tf.keras.layers.Dense(64, activation='relu')(X)
+    X = tf.keras.layers.Dropout(0.1)(X)
+    X = tf.keras.layers.Dense(16, activation='relu')(X)
+    y = tf.keras.layers.Dense(categories, activation='softmax', name='outputs')(X)
+    # if you are going to adjust the inner workings of the classification head, do so here.
+
+    model = tf.keras.Model(inputs=(input_ids, mask), outputs=[y])
+    model.layers[2].trainable = False
+
+    return model
+
+def create_tokenizer(model):
+    if model == "ProtBERT_BFD":
+        tokenizer = AutoTokenizer.from_pretrained('Rostlab/prot_bert_bfd', do_lower_case=False)
+    elif model == "XLNET":
+        tokenizer = XLNetTokenizer.from_pretrained("Rostlab/prot_xlnet", do_lower_case=False)
+    else:
+        print("invalid arg")
+        # throw error
+    return tokenizer
