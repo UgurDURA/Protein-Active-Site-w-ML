142927ffcda07fbbb55c57c64c25fa7662de16c2
selincandemir <candemir.selin@hotmail.com>
2021-07-11 23:14:40 +0300

    wip

--- Modules/Model_Spec/defaults.py
+++ Modules/Model_Spec/defaults.py
@@ -1,11 +1,16 @@
-import os
+
+import file_paths
 
 # default variables to use for hyperparameters and settings across all scripts
 MAX_LEN = 512       # max length of the input AA sequence
 BATCH_SIZE = 16     # Possible Values: 4/8/16/32
 DATA_SIZE = 50      # the number of total entries (train + val)
 
-# add to __all__ when you add new variables.
-__all__ = ["MAX_LEN", "BATCH_SIZE", "DATA_SIZE"]
+# ROOT_DIR = Path(__file__).parent.parent.parent  # This is your Project Root
+# SQLite_DB_PATH = str(os.path.join(ROOT_DIR, r'/[DATA]/Enzymes.db'))
+# UniProt_XML_PATH = os.path.join(ROOT_DIR, r'/[DATA]/uniprot/uniprot_sprot.xml')
 
-print(os.path.abspath())
+# add to __all__ when you add new variables.
+__all__ = ["MAX_LEN", "BATCH_SIZE", "DATA_SIZE"]    # , "SQLite_DB_PATH", UniProt_XML_PATH]
+
+print(file_paths.SQLite_DB_PATH)
--- file_paths.py
+++ file_paths.py
@@ -1,5 +1,13 @@
+from pathlib import Path
 import os
 
-SQLite_DB_PATH = os.path.abspath(r'../[DATA]/Enzymes.db')
-UniProt_XML_PATH = os.path.abspath(r'../[DATA]/uniprot/uniprot_sprot.xml')
 
+# ROOT_DIR = Path(__file__).parent.parent.parent  # This is your Project Root
+# SQLite_DB_PATH = str(os.path.join(ROOT_DIR, r'/[DATA]/Enzymes.db'))
+# UniProt_XML_PATH = os.path.join(ROOT_DIR, r'/[DATA]/uniprot/uniprot_sprot.xml')
+
+
+SQLite_DB_PATH = os.path.abspath(r'../../[DATA]/Enzymes.db')
+UniProt_XML_PATH = os.path.abspath(r'../../[DATA]/uniprot/uniprot_sprot.xml')
+
+print(SQLite_DB_PATH)
