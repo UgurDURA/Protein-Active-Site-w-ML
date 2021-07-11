df1a5709f0f803d8498a1481440f653842e0f26a
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2021-07-11 21:23:50 +0300

    Main updated
    Model saved and load succesfully

--- Modules/EC_Prediction_Modules/Main.py
+++ Modules/EC_Prediction_Modules/Main.py
@@ -15,14 +15,8 @@ Requested_sequence=input('Please request a sequence: ')
 
 
 
-
-
-
-
 def prepare(Requested_sequence):
 
-    Xids = np.zeros((1,256))
-    Xmask = np.zeros((1,256))
     iterable = Requested_sequence  
     separator = " "
     Requested_sequence=separator.join(iterable)
@@ -31,16 +25,27 @@ def prepare(Requested_sequence):
     tokenizer = AutoTokenizer.from_pretrained('Rostlab/prot_bert_bfd', do_lower_case=False, )
 
     Requested_sequence=re.sub(r"[UZOB]", "X",  Requested_sequence)
-    tokens = tokenizer(Requested_sequence, max_length=256, truncation=True, padding="max_length",
+    tokens = tokenizer(Requested_sequence, max_length=512, truncation=True, padding="max_length",
                                    add_special_tokens=True, return_token_type_ids=False, return_attention_mask=True, return_tensors='tf')
 
-    Xids[0, :], Xmask[0, :]  = tokens['input_ids'], tokens['attention_mask']
 
-    return Xids, Xmask
+    return {
+        'input_ids': tf.cast(tokens['input_ids'], tf.float64),
+        'attention_mask': tf.cast(tokens['attention_mask'], tf.float64)
+    }
 
 
-print(prepare(Requested_sequence))
 
-# model= tf.keras.models.load_model("")
+test=prepare(Requested_sequence)
 
-# prediction=model.predict([prepare()])
+model=tf.keras.models.load_model('EC_Prediction')
+
+model.summary()
+
+result=model.predict(test)
+
+print(result)
+
+result=np.argmax(result[0])
+
+print(result)
